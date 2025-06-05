ecaed3ea6cdb1d90e965f87334bac2f5eb395276
tuanaelvan <t.elvanoglu22@gmail.com>
2025-06-05 03:06:19 +0300

    Radiological results table was added, its contents were edited, sample data and photographs were added.

---
+++ ENabız/RadiologyData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENabız
+{
+    public class RadiologyData
+    {
+        public string Tarih { get; set; }
+        public string OnIzleme { get; set; }
+        public string HastaneAdi { get; set; }
+        public string Aciklama { get; set; }
+        public string Rapor { get; set; }
+    }
+}
--- ENabız/RadiologyPage.xaml.cs
+++ ENabız/RadiologyPage.xaml.cs
@@ -15,20 +15,101 @@ using System.Windows.Shapes;
 
 namespace ENabız
 {
-    /// <summary>
-    /// Interaction logic for RadiologyPage.xaml
-    /// </summary>
     public partial class RadiologyPage : Window
     {
         public RadiologyPage()
         {
             InitializeComponent();
+            Loaded += RadiologyPage_Loaded;
         }
 
-        private void DataGrid_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        private void RadiologyPage_Loaded(object sender, RoutedEventArgs e)
         {
-            // Seçim olayı için kod yazılabilir
+            List<RadiologyData> data = new List<RadiologyData>
+            {
+                new RadiologyData
+                {
+                    Tarih = "28.06.2024",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\VertebraGrafisi.jpg",
+                    HastaneAdi = "TRABZON YAVUZ SELİM KEMİK HASTANESİ",
+                    Aciklama = "Vertebra grafileri, dorsal veya lomber (İki yön )",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "21.12.2023",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\SağDizGrafisi.jpg",
+                    HastaneAdi = "KARADENİZ TEKNİK ÜNİVERSİTESİ FARABİ HASTANESİ",
+                    Aciklama = "Sağ diz grafisi (İki yön)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "12.06.2023",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\SinüsParanazalGrafisi.jpg",
+                    HastaneAdi = "ESKİŞEHİR ŞEHİR HASTANESİ",
+                    Aciklama = "Sinüs paranasal grafisi (Waters ve Caldwell)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "24.05.2023",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\ToraksGrafisi.jpg",
+                    HastaneAdi = "BİLECİK BOZÜYÜK DEVLET HASTANESİ",
+                    Aciklama = "Toraks grafisi (AP ve lateral)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "24.05.2023",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\AyaktaDirektBatınGrafisi.jpg",
+                    HastaneAdi = "BİLECİK BOZÜYÜK DEVLET HASTANESİ",
+                    Aciklama = "Ayakta direkt batın grafisi",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "24.05.2023",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\AbdominalGrafi.jpg",
+                    HastaneAdi = "BİLECİK BOZÜYÜK DEVLET HASTANESİ",
+                    Aciklama = "Abdominal grafi (Supin pozisyon)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "18.10.2019",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\ElBileğiGrafiği.jpg",
+                    HastaneAdi = "ESKİŞEHİR ŞEHİR HASTANESİ",
+                    Aciklama = "El bileği grafisi (PA ve lateral)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "02.01.2017",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\SakrokoksigealGrafisi.jpg",
+                    HastaneAdi = "TRABZON YAVUZ SELİM KEMİK HASTANESİ",
+                    Aciklama = "Sakrokoksigeal grafi (Tek yön)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "26.11.2016",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\LumbalGrafisi.jpg",
+                    HastaneAdi = "KARADENİZ TEKNİK ÜNİVERSİTESİ FARABİ HASTANESİ",
+                    Aciklama = "Vertebra grafileri, lomber bölge (İki yön)",
+                    Rapor = "Yok"
+                },
+                new RadiologyData
+                {
+                    Tarih = "17.06.2016",
+                    OnIzleme = "C:\\Users\\TUANA\\OneDrive\\Masaüstü\\Projects\\ENabız\\ENabız\\ENabız\\mrornekleri\\SolKalçaGrafisi.jpg",
+                    HastaneAdi = "ESKİŞEHİR ŞEHİR HASTANESİ",
+                    Aciklama = "Sol kalça grafisi (AP)",
+                    Rapor = "Yok"
+                },
+            };
+
+            RadiologyDataGrid.ItemsSource = data;
         }
     }
-
 }
